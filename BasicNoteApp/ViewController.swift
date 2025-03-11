f9b7929f97193130a6e4c52f142847c763a9b897
Melisa Melayim <melisamelayim@hotmail.com>
2025-03-11 10:01:26 +0300

    Initial Commit

---
+++ BasicNoteApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BasicNoteApp
+//
+//  Created by Max on 11.03.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
